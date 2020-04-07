90c9001abb34b2b2e53197180fe52c5044c21cd5
Himanshu3101 <himanshusrivastava3101@gmail.com>
2020-04-07 13:24:47 +0530

    Complete Asset Tagging 07/04/2020

--- app/src/main/java/com/assettagging/test/model/WebServer/APIClient.java
+++ app/src/main/java/com/assettagging/test/model/WebServer/APIClient.java
@@ -12,7 +12,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class APIClient {
 
     private static Retrofit retrofit = null;
-    private static String BaseUrl = "http://192.168.200.199:804/service1.svc/";
+//    Testing URL
+    private static String BaseUrl = "http://219.90.65.215:9003/service1.svc/";
+//    123456   123456
+
+//    Live URL
+//    private static String BaseUrl = "http://192.168.200.182:8082/service1.svc/";
+
    // private static String BaseUrl = "http://192.168.1.246:804/service1.svc/";
     private static OkHttpClient client;
 
